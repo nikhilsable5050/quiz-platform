4bf3b7f6058cfc2824b8eb0035ec4861ce1c25e5
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-07-16 18:54:51 +0530

    Initial project setup

---
+++ monolith/src/main/java/nikhilsable/quiz_platform/QuizPlatformApplication.java
@@ -0,0 +1,13 @@
+package nikhilsable.quiz_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizPlatformApplication.class, args);
+	}
+
+}
---
+++ monolith/src/test/java/nikhilsable/quiz_platform/QuizPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package nikhilsable.quiz_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
