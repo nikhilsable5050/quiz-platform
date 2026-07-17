29a77d3f76f32af190fafccef49a05bc41ee424d
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-07-17 15:14:26 +0530

    Update get all questions controller

--- monolith/src/main/java/nikhilsable/quiz_platform/controller/QuestionController.java
+++ monolith/src/main/java/nikhilsable/quiz_platform/controller/QuestionController.java
@@ -1,14 +1,22 @@
 package nikhilsable.quiz_platform.controller;
 
+import nikhilsable.quiz_platform.model.Question;
+import nikhilsable.quiz_platform.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("question")
 public class QuestionController {
+    @Autowired
+    QuestionService questionService;
+
     @GetMapping("allQuestions")
-    public String getAllQuestions(){
-        return "Hi, these are your question";
+    public List<Question> getAllQuestions(){
+        return questionService.getAllQuestions();
     }
 }
--- monolith/src/main/java/nikhilsable/quiz_platform/service/QuestionService.java
+++ monolith/src/main/java/nikhilsable/quiz_platform/service/QuestionService.java
@@ -1,4 +1,19 @@
 package nikhilsable.quiz_platform.service;
 
+import nikhilsable.quiz_platform.dao.QuestionDao;
+import nikhilsable.quiz_platform.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public List<Question> getAllQuestions() {
+        return questionDao.findAll();
+    }
 }
