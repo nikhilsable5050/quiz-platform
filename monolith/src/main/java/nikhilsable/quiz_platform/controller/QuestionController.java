c6fb4ffa3c444afc735a1be947b67070f0baed01
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-07-17 15:53:35 +0530

    Implement search by category

--- monolith/src/main/java/nikhilsable/quiz_platform/controller/QuestionController.java
+++ monolith/src/main/java/nikhilsable/quiz_platform/controller/QuestionController.java
@@ -4,6 +4,7 @@ import nikhilsable.quiz_platform.model.Question;
 import nikhilsable.quiz_platform.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,10 @@ public class QuestionController {
     @GetMapping("allQuestions")
     public List<Question> getAllQuestions(){
         return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
     }
 }
