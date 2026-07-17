da55fd2c5935289b3057c73765bf863ecc937081
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-07-17 12:35:04 +0530

    Refactor controller

--- monolith/src/main/java/nikhilsable/quiz_platform/controller/QuestionController.java
+++ monolith/src/main/java/nikhilsable/quiz_platform/controller/QuestionController.java
@@ -1,4 +1,4 @@
-package nikhilsable.quiz_platform;
+package nikhilsable.quiz_platform.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
