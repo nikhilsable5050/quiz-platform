ae8a75535b0aa9934d6345bba0a4ea8079550172
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-07-24 19:36:20 +0530

    Use ResponseEntity in QuestionController

--- monolith/src/main/java/nikhilsable/quiz_platform/controller/QuestionController.java
+++ monolith/src/main/java/nikhilsable/quiz_platform/controller/QuestionController.java
@@ -3,28 +3,34 @@ package nikhilsable.quiz_platform.controller;
 import nikhilsable.quiz_platform.model.Question;
 import nikhilsable.quiz_platform.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("question")
+@RequestMapping("/question")
 public class QuestionController {
+
     @Autowired
-    QuestionService questionService;
+    private QuestionService questionService;
 
-    @GetMapping("allQuestions")
-    public List<Question> getAllQuestions(){
-        return questionService.getAllQuestions();
+    @GetMapping("/allQuestions")
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        return ResponseEntity.ok(questionService.getAllQuestions());
     }
 
-    @GetMapping("category/{category}")
-    public List<Question> getQuestionsByCategory(@PathVariable String category){
-        return questionService.getQuestionsByCategory(category);
+    @GetMapping("/category/{category}")
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category) {
+        return ResponseEntity.ok(questionService.getQuestionsByCategory(category));
     }
 
-    @PostMapping("add")
-    public String addQuestion(@RequestBody Question question){
-        return questionService.addQuestion(question);
+    @PostMapping("/add")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question) {
+
+        String response = questionService.addQuestion(question);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 }
